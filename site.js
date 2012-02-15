190b1f88a99ee043e679912498ad3f6672d790cc
tristen <tristen@tristen.ca>
2012-02-15 09:55:37 -0500

    Title change adding in search json and wrapping code in a singleton

--- site.js
+++ site.js
@@ -1,22 +1,28 @@
-var mm = com.modestmaps;
-var url = 'http://api.tiles.mapbox.com/v3/saman.map-kg3gj8s6.jsonp';
+(function(context) {
+var maki = {};
 
-wax.tilejson(url, function(tilejson) {
-    tilejson.minzoom = 16;
-    tilejson.maxzoom = 18;
-    var m = new mm.Map('map',
-        new wax.mm.connector(tilejson), null, [
-            new mm.DragHandler(),
-            new mm.TouchHandler()
-        ]
-    );
+maki.map = function() {
+    var mm = com.modestmaps;
+    var url = 'http://api.tiles.mapbox.com/v3/saman.map-kg3gj8s6.jsonp';
 
-    m.setCenterZoom(new mm.Location(38.91710,-77.03024), 17);
-    wax.mm.zoomer(m).appendTo($('#controls')[0]);
-    wax.mm.interaction(m);
-});
+    wax.tilejson(url, function(tilejson) {
+        tilejson.minzoom = 16;
+        tilejson.maxzoom = 18;
+        var m = new mm.Map('map',
+            new wax.mm.connector(tilejson), null, [
+                new mm.DragHandler(),
+                new mm.TouchHandler()
+            ]
+        );
 
-$(function () {
+        m.setCenterZoom(new mm.Location(38.91710,-77.03024), 17);
+        wax.mm.zoomer(m).appendTo($('#controls')[0]);
+        wax.mm.interaction(m);
+    });
+};
+$(maki.map);
+
+maki.slideshow = function() {
     $('#maki-icon-preview').find('a').click(function (e) {
         e.preventDefault();
         if (!$(this).hasClass('active')) {
@@ -30,18 +36,28 @@ $(function () {
             $(this).addClass('active');
         }
     });
+};
+$(maki.slideshow);
 
+maki.search = function () {
     // Handle geocoder form submission
     var input = $('.canvas-controls').find('input');
     var icons = $('.maki-set');
+
+
+    input.focus(function() {
+        icons.stop().animate({
+            opacity: 0.35
+        }, 500);
+    });
 
     input.blur(function() {
         icons.stop().animate({
             opacity: 100
         }, 500);
-    }).focus(function() {
-        icons.stop().animate({
-            opacity: 0.35
-        }, 500);
     });
-});
+};
+$(maki.search);
+
+window.maki = maki;
+})(window);
