a6ca9b8896a6ad025e40e9e6a16b63dc63c956b1
Alex Buchanan <buchanae@gmail.com>
2015-11-26 19:08:32 +1100

    WIP: prototype gulp build system

--- sdf-render.js
+++ sdf-render.js
@@ -1,11 +1,9 @@
-#!/usr/bin/env node
-
-var glob = require('glob');
-var fs = require('fs');
+var path = require('path');
 var jsdom = require('jsdom').jsdom;
 var SvgPath = require('svgpath');
 var sdf = require('fontnik').pathToSDF;
 var PNG = require('pngjs').PNG;
+var through = require('through2');
 
 var buffer = 3;
 var cutoff = 2/8;
@@ -15,71 +13,98 @@ var w = 24,
     wb = w + 2*buffer,
     hb = h + 2*buffer;
 
-glob('src/*-24.svg', function (err, files) {
-    if (err) throw err;
-    files.forEach(function (file) {
-        var name = file.match(/src\/(.*)-24\.svg/)[1];
-        var doc = jsdom(fs.readFileSync(file).toString());
 
-        var svg = doc.getElementsByTagName('svg')[0];
-        var paths = doc.getElementsByTagName('path');
-        for (var i = 0; i < paths.length; i++) {
-            var path = paths[i];
+function renderFile(file) {
+  var doc = jsdom(file.contents);
 
-            if ((!path.attributes.style || !/fill:#(44|45){3}/.test(path.attributes.style.value)) &&
-                (!path.attributes.fill || !/#(44|45){3}/.test(path.attributes.fill.value)))
-                continue;
+  var svg = doc.getElementsByTagName('svg')[0];
+  var paths = doc.getElementsByTagName('path');
 
-            var svgPath = new SvgPath(path.attributes.d.value).abs().unshort();
+  for (var i = 0; i < paths.length; i++) {
+      var path = paths[i];
 
-            if (path.attributes.transform)
-                svgPath.transform(path.attributes.transform.value);
+      if ((!path.attributes.style || !/fill:#(44|45){3}/.test(path.attributes.style.value)) &&
+          (!path.attributes.fill || !/#(44|45){3}/.test(path.attributes.fill.value)))
+          continue;
 
-            var parent = path.parentElement;
-            while (parent.tagName === 'G') {
-                if (parent.attributes.transform) {
-                    svgPath.transform(parent.attributes.transform.value);
-                }
-                parent = parent.parentElement;
-            }
+      var svgPath = new SvgPath(path.attributes.d.value).abs().unshort();
 
-            if (svg.attributes.viewBox) {
-                var viewBox = svg.attributes.viewBox.value.split(/\s+/);
-                svgPath.translate(-viewBox[0], -viewBox[1]);
-            }
+      if (path.attributes.transform)
+          svgPath.transform(path.attributes.transform.value);
 
-            var commands = svgPath.segments.map(function(segment) {
-                switch (segment[0]) {
-                    case 'M':
-                    case 'L':
-                        return {type: segment[0], x: segment[1], y: segment[2]};
-                    case 'Q':
-                        return {type: segment[0], x1: segment[1], y1: segment[2], x: segment[3], y: segment[4]};
-                    case 'C':
-                        return {type: segment[0], x1: segment[1], y1: segment[2], x2: segment[3], y2: segment[4], x: segment[5], y: segment[6]};
-                    case 'Z':
-                        return {type: segment[0]};
-                    default:
-                        throw new Error('Unknown command: ' + segment[0]);
-                }
-            });
+      var parent = path.parentElement;
+      while (parent.tagName === 'G') {
+          if (parent.attributes.transform) {
+              svgPath.transform(parent.attributes.transform.value);
+          }
+          parent = parent.parentElement;
+      }
 
-            var data = sdf(commands, w, h, buffer, cutoff);
-            var png = new PNG({width: wb, height: hb});
+      if (svg.attributes.viewBox) {
+          var viewBox = svg.attributes.viewBox.value.split(/\s+/);
+          svgPath.translate(-viewBox[0], -viewBox[1]);
+      }
 
-            for (var j = 0; j < wb * hb; j++) {
-                png.data[j*4]   = 0;
-                png.data[j*4+1] = 0;
-                png.data[j*4+2] = 0;
-                png.data[j*4+3] = data[j];
-            }
+      var commands = svgPath.segments.map(function(segment) {
+          switch (segment[0]) {
+              case 'M':
+              case 'L':
+                  return {
+                    type: segment[0],
+                    x: segment[1],
+                    y: segment[2]
+                  };
+              case 'Q':
+                  return {
+                    type: segment[0],
+                    x1: segment[1],
+                    y1: segment[2],
+                    x: segment[3],
+                    y: segment[4]
+                  };
+              case 'C':
+                  return {
+                    type: segment[0],
+                    x1: segment[1],
+                    y1: segment[2],
+                    x2: segment[3],
+                    y2: segment[4],
+                    x: segment[5],
+                    y: segment[6]
+                  };
+              case 'Z':
+                  return {type: segment[0]};
+              default:
+                  throw new Error('Unknown command: ' + segment[0]);
+          }
+      });
 
-            png.pack()
-                .pipe(fs.createWriteStream('sdf/' + name + '.png'));
+      var data = sdf(commands, w, h, buffer, cutoff);
+      var png = new PNG({width: wb, height: hb});
 
-            return;
-        }
+      for (var j = 0; j < wb * hb; j++) {
+          png.data[j*4]   = 0;
+          png.data[j*4+1] = 0;
+          png.data[j*4+2] = 0;
+          png.data[j*4+3] = data[j];
+      }
 
-        console.error('Couldn\'t find path for ' + name);
+      return png.pack();
+      console.log(png.pack());
+      png.pack().pipe(file);
+      return file;
+  }
+}
+
+module.exports = function() {
+  return through.obj(function(file, encoding, callback) {
+    var buffers = [];
+    var pngStream = renderFile(file);
+
+    pngStream.on('data', function(d) { buffers.push(d); });
+    pngStream.on('end', function() {
+      file.contents = Buffer.concat(buffers);
+      callback(null, file);
     });
-});
+  });
+};
