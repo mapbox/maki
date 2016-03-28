9b352d3829a6f76b5068cdb8f92c8e028d8c799e
samanpwbb <samanpwbb@gmail.com>
2016-03-28 19:43:35 -0400

    maki 2.0

--- test/maki.test.js
+++ test/maki.test.js
@@ -1,68 +1,84 @@
 var fs = require('fs'),
-    test = require('tap').test,
-    https = require('https'),
-    jsdom = require('jsdom');
+    path = require('path'),
+    test = require('tape'),
+    xml2js = require('xml2js');
 
-test('Maki', function(t) {
-    t.doesNotThrow(function() {
-        JSON.parse(fs.readFileSync(__dirname + '/../_includes/maki.json'));
-    }, 'JSON is invalid');
+var parseString = xml2js.parseString;
 
-    var features = JSON.parse(fs.readFileSync(__dirname + '/../_includes/maki.json'));
 
-    features.forEach(function(f) {
-        t.equal(typeof f.name, 'string', 'name property');
-        t.equal(typeof f.icon, 'string', 'icon property');
-        t.equal(typeof f.tags, 'object', 'tags property');
-        [12, 18, 24].forEach(function(size) {
-            t.doesNotThrow(function() {
-                fs.statSync(__dirname + '/../src/' + f.icon + '-' + size + '.svg');
-                fs.statSync(__dirname + '/../renders/' + f.icon + '-' + size + '.png');
-                fs.statSync(__dirname + '/../renders/' + f.icon + '-' + size + '@2x.png');
-            }, 'source file present');
-            jsdom.env({
-                html: fs.readFileSync(__dirname + '/../src/' + f.icon + '-' + size + '.svg'),
-                done: function(errors, window) {
-                    t.equal(
-                        'rect',
-                        window.document.getElementById('canvas')._tagName,
-                        'SVG contains <rect> with id "canvas"'
-                    );
-                    t.equal(
-                        'fill:none;stroke:none;visibility:hidden',
-                        window.document.getElementById('canvas').getAttribute('style'),
-                        'canvas element is hidden'
-                    );
-                    t.equal(
-                        size.toString(),
-                        window.document.getElementById('canvas').getAttribute('width'),
-                        'canvas width matches icon size'
-                    );
-                    t.equal(
-                        size.toString(),
-                        window.document.getElementById('canvas').getAttribute('height'),
-                        'canvas height matches icon size.'
-                    );
-                }
-            });
-        });
+test('valid svgs', function(t) {
+
+  fs.readdir('./icons/', function(err, files) {
+    var svgFiles = files.filter(function(file) {
+      return file.split('.').pop().indexOf('svg') !== -1;
     });
 
-    t.end();
-});
+    svgFiles.forEach(function(fileName, j) {
+      function endsWith(str, test) {
+        return str.lastIndexOf(test) === (str.length - test.length);
+      }
 
-test('production endpoint', function(t) {
-    var body = '';
-    https.get('https://www.mapbox.com/maki/www/maki.json', function(res) {
-        t.equal(res.statusCode, 200, 'HTTP=' + res.statusCode);
-        res.on('data', function(chunk) {
-            body += chunk;
-        });
-        res.on('end', function() {
-            t.doesNotThrow(function() {
-                JSON.parse(body.toString());
-            }, 'JSON is valid');
-            t.end();
-        });
+      t.ok(endsWith(fileName,'-11.svg') || endsWith(fileName,'-15.svg'), fileName + ' filename ends with "-11.svg or "-15.svg"');
+
+      fs.readFile('./icons/' + fileName, 'utf8', function(err, file) {
+          if (err) t.fail(err);
+          parseString(file, function(err, parsed) {
+            if (err) t.fail(err);
+            validateSvg(parsed.svg, fileName);
+          });
+      });
+      if ((j+1) === svgFiles.length) {
+        t.end();
+      }
     });
+  });
+
+  function validateSvg(svg, fileName) {
+    var errors = [],
+        height = parseFloat(svg.$.height),
+        width = parseFloat(svg.$.width);
+
+    function invalidElement(o) {
+      var keys = Object.keys(o),
+        invalid = false;
+
+      keys.some(function(key) {
+        invalid = key.match(/^(rectangle|circle|ellipse|line|polyline|polygon)$/) && key;
+        return invalid;
+      });
+      return invalid;
+    };
+
+    function checkPaths(pathArray) {
+      pathArray.forEach(function(path) {
+        if (path.$.transform) errors.push('transformed paths');
+      });
+    };
+
+    function traverseGroups(groupArray) {
+      groupArray.forEach(function(group) {
+        if (group.$.transform) errors.push('transformed groups');
+        if (invalidElement(group)) errors.push('has ' + invalidElement(group));
+        if (group.path) {
+          checkPaths(group.path);
+        } else if (group.g) {
+          traverseGroups(group.g);
+        }
+      });
+    };
+
+    if (!(height === 11 || height === 15) ||
+      !(width === 11 || width === 15) ||
+      height !== width) errors.push('invalid size');
+    if (parseFloat(svg.$.viewBox.split(' ')[2]) !== width ||
+      parseFloat(svg.$.viewBox.split(' ')[3]) !== height) errors.push('invalid viewBox');
+    if (invalidElement(svg)) errors.push('has ' + invalidElement(svg));
+    if (svg.g) traverseGroups(svg.g);
+    if (svg.path) checkPaths(svg.path);
+
+
+    t.notOk(errors.length, fileName + ' has ' + errors.length + ' errors' + (errors.length ? ':' : '') + errors.join(', '));
+
+  };
+
 });
