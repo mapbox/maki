3e8f2e1ee49e06de4509158335704cced662b0c0
tristen <tristen.brown@gmail.com>
2024-12-23 16:38:27 -0500

    Add build command for maki icons with metadata for styling

---
+++ scripts/make-meta-icons.js
@@ -0,0 +1,71 @@
+/*
+  Create icons with stroke and m:parameter options for 
+  custom icon authoring from our SDKs.
+ */
+const path = require('path');
+const { gatherIcons, write } = require('./utils');
+
+function metaMap({ data }) {
+  data['$'] = {
+    ...data['$'],
+    'xmlns:m': 'https://www.mapbox.com',
+    viewBox: '0 0 23 23',
+    height: '23',
+    width: '23'
+  };
+
+  data.path.forEach((p, index) => {
+    p.$ = {
+      ...p.$,
+      transform: 'translate(4 4)',
+      fill: '#000'
+    };
+
+    // Add a stroked shape to below by unshifting an item into the array
+    data.path.unshift({
+      $: {
+        ...data.path[index].$,
+        style: 'stroke-linejoin:round;stroke-miterlimit:4;',
+        transform: 'translate(4 4)',
+        stroke: '#fff',
+        'stroke-width': 4
+      }
+    });
+  });
+
+  data['m:metadata'] = {
+    ['m:parameters']: {
+      ['m:parameter']: [
+        {
+          $: {
+            ['m:name']: 'background',
+            ['m:type']: 'color',
+            ['m:value']: '#000'
+          }
+        },
+        {
+          $: {
+            ['m:name']: 'stroke',
+            ['m:type']: 'color',
+            ['m:value']: '#fff'
+          }
+        }
+      ]
+    }
+  };
+
+  return data;
+}
+
+function makeMetaIcons() {
+  return gatherIcons(path.join(__dirname, '../icons')).then(svgs => {
+    const metaMappedIcons = svgs.map(metaMap);
+    write(metaMappedIcons, path.join(__dirname, '../meta-icons'));
+  });
+}
+
+module.exports = makeMetaIcons;
+
+if (require.main === module) {
+  makeMetaIcons();
+}
---
+++ scripts/utils.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+const pify = require('pify');
+const xml2js = require('xml2js');
+const mkdirp = require('mkdirp');
+
+const builder = new xml2js.Builder({
+  rootName: 'svg',
+  xmldec: { version: '1.0', encoding: 'UTF-8' }
+});
+
+const pReadFile = pify(fs.readFile);
+const pReaddir = pify(fs.readdir);
+
+function gatherIcons(iconPath) {
+  return pReaddir(iconPath).then(files => {
+    const svgFiles = files.filter(f => f.indexOf('.svg') !== -1);
+
+    return Promise.all(
+      svgFiles.map(f => pReadFile(path.join(iconPath, f), 'utf8'))
+    )
+      .then(svgs => {
+        return Promise.all(
+          svgs.map(svg => {
+            // Need to create a parser for each svg
+            const parser = new xml2js.Parser();
+            const pParse = pify(parser.parseString);
+            return pParse(svg);
+          })
+        );
+      })
+      .then(parsedSvgs => {
+        return parsedSvgs.map((pSvg, i) => ({
+          fileName: svgFiles[i],
+          data: pSvg.svg
+        }));
+      });
+  });
+}
+
+function write(cleanedSvgs, iconPath) {
+  const pWrite = pify(fs.writeFile);
+  return pify(mkdirp)(iconPath).then(
+    Promise.all(
+      cleanedSvgs.map(svgObj => {
+        const svg = builder.buildObject(svgObj);
+        pWrite(path.join(iconPath, `${svgObj.$.id}.svg`), svg, 'utf8');
+      })
+    )
+  );
+}
+
+module.exports = {
+  write,
+  gatherIcons
+};
